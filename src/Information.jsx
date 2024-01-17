2050acb41b1b8d8f4f8dedb893626360ae7fd19b
Rabin Thami <rabinthami36@gmail.com>
2024-01-17 08:56:50 +0545

    first commit

---
+++ src/Information.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css'
+
+function InfoSection() {
+    return (
+        <section className="info__section">
+            hhh
+        </section>
+    )
+}
+
+
+export default InfoSection
